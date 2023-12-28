39af57c696e1b8f39895ba93c8cf505cf19c661c
Saravana <saravanakumar@facilio.com>
2023-12-29 03:12:11 +0530

    vendor api initial commit practice

---
+++ springboot-backend/src/main/java/net/javaguides/vendors/model/Vendor.java
@@ -0,0 +1,55 @@
+package net.javaguides.vendors.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "vendors")
+public class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    public Vendor() {
+
+    }
+    public Vendor(String firstName, String lastName, String emailId) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
